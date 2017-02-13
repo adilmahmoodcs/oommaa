46db1a7f9395a2d57eb466688b951ba0d7ebb16f
Alessandro Lepore <lepore.ale@gmail.com>
2017-02-13 14:20:25 +0100

    update page searcher spec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,7 +30,7 @@ require 'webmock/rspec'
 ActiveRecord::Migration.maintain_test_schema!
 
 VCR.configure do |config|
-  config.cassette_library_dir = "spec/vcr_cassettes"
+  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
 end
 
--- spec/services/fb_page_searcher_spec.rb
+++ spec/services/fb_page_searcher_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe FBPageSearcher do
   subject { FBPageSearcher }
+  let(:token) { "EAAIUKbXn8xsBAPsYb2xKl4fcrpDnJKQZCrZB2EuLjHyg4GrXrlqnd1xHROqV3DFFbPRuzILTEwtoXLr5fo8PTxvvyN68uPUpbvCwnoIPrdJLmmKkuJXd2dMGekT8THoc2eZBuftIVXaLTN656NYzpxOu9FtG10ZD" }
 
   it "raise ArgumentError with missing arguments" do
     expect{subject.new}.to raise_error(ArgumentError)
@@ -11,12 +12,15 @@ RSpec.describe FBPageSearcher do
   end
 
   describe "#call" do
-    it "return an array of facebook pages" do
+    it "return an array of facebook pages data" do
       result = VCR.use_cassette("fb_page_searcher") do
-        subject.new(term: "Dallas Cowboys", token: "some-token").call
+        subject.new(term: "Dallas Cowboys", token: token).call
       end
 
-      expect(result).to be_a(Array)
+      expect(result.first["id"]).to be_present
+      expect(result.first["name"]).to be_present
+      expect(result.first["link"]).to be_present
+      expect(result.first["picture"]).to be_present
     end
   end
 end
