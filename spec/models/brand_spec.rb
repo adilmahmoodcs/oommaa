963ff2823e7461269cf0950db0ca8a434d5fc09d
Alessandro Lepore <lepore.ale@gmail.com>
2017-02-13 15:27:03 +0100

    test not having callbacks on brand update

--- spec/models/brand_spec.rb
+++ spec/models/brand_spec.rb
@@ -3,7 +3,13 @@ require 'rails_helper'
 RSpec.describe Brand, type: :model do
   let(:brand) { create(:brand) }
 
-  it "starts pages importer after creation" do
+  it "starts pages import after creation" do
     expect{ brand.run_callbacks(:commit) }.to have_enqueued_job(BrandPagesImporterJob)#.with(brand) # FIXME
+  end
+
+  it "do not start import after update" do
+    brand.run_callbacks(:commit) # after create
+    brand.update_attribute(:name, "some name")
+    expect{ brand.run_callbacks(:commit) }.to_not have_enqueued_job(BrandPagesImporterJob)
   end
 end
