460d9c321ab1c65289d22ce3649032371e8d4123
Alessandro Lepore <lepore.ale@gmail.com>
2017-02-13 15:22:42 +0100

    mount sidekiq UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   resources :keywords
   resources :brands
 
   devise_for :users
+  mount Sidekiq::Web => '/sidekiq'
 
   root to: "keywords#index"
 end
