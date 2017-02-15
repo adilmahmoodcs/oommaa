e79a84ee24acff9f9b7639656b6947d1a2c1e25b
Alessandro Lepore <lepore.ale@gmail.com>
2017-02-15 12:45:53 +0100

    update capistrano conf

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,17 +1,13 @@
 # config valid only for current version of Capistrano
-lock "3.7.1"
+lock "3.7.2"
 
 set :application, "counterfind"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :repo_url, "git@github.com:counterfind/counterfind-rails.git"
 set :deploy_to, "/var/www/#{fetch(:application)}_#{fetch(:stage)}"
 set :log_level, :debug
 
-set :linked_dirs, fetch(:linked_dirs, []) + %w[
-  log tmp/pids tmp/cache tmp/sockets vendor/bundle public/sitemaps
-]
-
 # append :linked_files, "config/database.yml", "config/secrets.yml"
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
 set :rvm_type, :user
 set :rvm_ruby_version, '2.4.0'
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,10 @@
 set :puma_tag, "Counterfind Production"
 set :puma_threads, [0, 4]
 set :puma_workers, 2
+
+server "ec2-54-234-172-199.compute-1.amazonaws.com",
+  user: "deployer",
+  roles: [:app, :web, :db, :sidekiq],
+  ssh_options: {
+    forward_agent: true
+  }
