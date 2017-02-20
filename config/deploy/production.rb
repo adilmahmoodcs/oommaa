1d7f6217d6b0709f2d3aa0636c77b9dbfa9906ef
Alessandro Lepore <lepore.ale@gmail.com>
2017-02-20 18:18:33 +0100

    update deploy url

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,7 +2,7 @@ set :puma_tag, "Counterfind Production"
 set :puma_threads, [0, 4]
 set :puma_workers, 2
 
-server "ec2-54-234-172-199.compute-1.amazonaws.com",
+server "dev.counterfind.com",
   user: "deployer",
   roles: [:app, :web, :db, :sidekiq],
   ssh_options: {
