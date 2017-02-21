27643c9f6c902001e0c8809c4cb099246e31a220
Alessandro Lepore <lepore.ale@gmail.com>
2017-02-21 14:41:03 +0100

    add missing deploy linked dirs

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :deploy_to, "/var/www/#{fetch(:application)}_#{fetch(:stage)}"
 set :log_level, :debug
 
 # append :linked_files, "config/database.yml", "config/secrets.yml"
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
 set :rvm_type, :user
 set :rvm_ruby_version, '2.4.0'
