da02aecbb06e15437577b0a0ae675e00173bbbdc
Alessandro Lepore <lepore.ale@gmail.com>
2017-02-28 16:55:46 +0100

    fix post screenshots jobs

--- app/jobs/post_screenshots_job.rb
+++ app/jobs/post_screenshots_job.rb
@@ -9,23 +9,25 @@ class PostScreenshotsJob
 
     post.parse_all_links! if post.all_links.none?
 
+    save_screenshot!(post, post.permalink, AdScreenshot)
     post.all_links.each do |link|
-      klass = facebook?(link) ? AdScreenshot : ProductScreenshot
-      tmp_file_name = ScreenshotGrabber.new(link).call
-      return unless tmp_file_name
-
-      screenshot = klass.new
-      screenshot.facebook_post = post
-      screenshot.image = File.open(tmp_file_name)
-      screenshot.save!
-
-      # File.unlink(tmp_file_name)
+      save_screenshot!(post, link, ProductScreenshot)
     end
+
+    true
   end
 
   private
 
-  def facebook?(url)
-    url.match?(/https\:\/\/www\.facebook\.com\//)
+  def save_screenshot!(post, url, klass)
+    tmp_file_name = ScreenshotGrabber.new(url).call
+    return unless tmp_file_name
+
+    screenshot = klass.new
+    screenshot.facebook_post = post
+    screenshot.image = File.open(tmp_file_name)
+    screenshot.save!
+
+    # File.unlink(tmp_file_name)
   end
 end
