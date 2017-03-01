42b0ae50c89d6137f8029231dc3a82ea5c59f6ff
Alessandro Lepore <lepore.ale@gmail.com>
2017-03-01 12:23:33 +0100

    Revert "expand all URLs"
    This reverts commit c3fea0bf41f298529b97014b1e279ded1fea0121.

--- config/initializers/embiggen.rb
+++ config/initializers/embiggen.rb
@@ -1,12 +1,5 @@
-# Custom logic to attempt to expand every URI
-class Embiggen::ExpandEverything
-  def self.include?(*)
-    true
-  end
-end
-
 Embiggen.configure do |config|
   config.timeout = 10
   config.redirects = 4
-  config.shorteners = Embiggen::ExpandEverything
+  config.shorteners += %w[smarturl.it]
 end
