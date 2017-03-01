c3fea0bf41f298529b97014b1e279ded1fea0121
Alessandro Lepore <lepore.ale@gmail.com>
2017-03-01 10:30:19 +0100

    expand all URLs
    because is not possible to keep the shorteners lists updated

--- config/initializers/embiggen.rb
+++ config/initializers/embiggen.rb
@@ -1,5 +1,12 @@
+# Custom logic to attempt to expand every URI
+class Embiggen::ExpandEverything
+  def self.include?(*)
+    true
+  end
+end
+
 Embiggen.configure do |config|
   config.timeout = 10
   config.redirects = 4
-  config.shorteners += %w[smarturl.it]
+  config.shorteners = Embiggen::ExpandEverything
 end
