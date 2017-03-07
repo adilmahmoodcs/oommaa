a783ccdc43c8716c9dc4511476abf41fd8e7e188
Alessandro Lepore <lepore.ale@gmail.com>
2017-03-07 12:38:40 +0100

    do not authorize devise controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,8 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :devise_permitted_parameters, if: :devise_controller?
   # pundit checks
-  after_action :verify_authorized
-  after_action :verify_policy_scoped, only: :index
+  after_action :verify_authorized, unless: :devise_controller?
+  after_action :verify_policy_scoped, only: :index, unless: :devise_controller?
 
   private
 
