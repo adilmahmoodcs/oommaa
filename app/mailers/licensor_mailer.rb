8d96cdd664bd433633cc4871c0717ea2cc7c6868
Alessandro Lepore <lepore.ale@gmail.com>
2017-03-08 17:48:41 +0100

    licensor notify_reported_post email template

--- app/mailers/licensor_mailer.rb
+++ app/mailers/licensor_mailer.rb
@@ -1,5 +1,8 @@
 class LicensorMailer < ApplicationMailer
-  def notify_reported_post
+  def notify_reported_post(licensor, post)
+    return if licensor.main_contact.blank?
 
+    @post = post
+    mail(to: licensor.main_contact, subject: "CounterFind")
   end
 end
--- spec/mailers/previews/licensor_mailer_preview.rb
+++ spec/mailers/previews/licensor_mailer_preview.rb
@@ -1,4 +1,8 @@
 # Preview all emails at http://localhost:3000/rails/mailers/licensor_mailer
 class LicensorMailerPreview < ActionMailer::Preview
-
+  def notify_reported_post
+    licensor = Licensor.last
+    post = FacebookPost.last
+    LicensorMailer.notify_reported_post(licensor, post)
+  end
 end
