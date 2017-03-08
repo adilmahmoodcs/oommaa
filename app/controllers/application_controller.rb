fc3af9d19b0c417c7665c9a68997d2f6d7bd3787
Alessandro Lepore <lepore.ale@gmail.com>
2017-03-08 17:17:38 +0100

    handle authorization errors and display message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
   before_action :authenticate_user!
   before_action :devise_permitted_parameters, if: :devise_controller?
   # pundit checks
@@ -13,5 +15,10 @@ class ApplicationController < ActionController::Base
 
   def devise_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :licensor_id])
+  end
+
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_back(fallback_location: root_path)
   end
 end
