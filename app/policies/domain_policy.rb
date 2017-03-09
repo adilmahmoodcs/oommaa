70b55b003d504d1384b076c1bb9301bd24fb2a90
Alessandro Lepore <lepore.ale@gmail.com>
2017-03-09 15:40:46 +0100

    clients full auth for keywords and domains

--- app/policies/domain_policy.rb
+++ app/policies/domain_policy.rb
@@ -5,6 +5,30 @@ class DomainPolicy < ApplicationPolicy
     end
   end
 
+  def index?
+    user.admin? || user.confirmed_client?
+  end
+
+  def edit?
+    user.admin? || user.confirmed_client?
+  end
+
+  def update?
+    user.admin? || user.confirmed_client?
+  end
+
+  def new?
+    user.admin? || user.confirmed_client?
+  end
+
+  def create?
+    user.admin? || user.confirmed_client?
+  end
+
+  def destroy?
+    user.admin? || user.confirmed_client?
+  end
+
   def change_status?
     user.admin?
   end
--- app/policies/keyword_policy.rb
+++ app/policies/keyword_policy.rb
@@ -4,4 +4,28 @@ class KeywordPolicy < ApplicationPolicy
       scope
     end
   end
+
+  def index?
+    user.admin? || user.confirmed_client?
+  end
+
+  def edit?
+    user.admin? || user.confirmed_client?
+  end
+
+  def update?
+    user.admin? || user.confirmed_client?
+  end
+
+  def new?
+    user.admin? || user.confirmed_client?
+  end
+
+  def create?
+    user.admin? || user.confirmed_client?
+  end
+
+  def destroy?
+    user.admin? || user.confirmed_client?
+  end
 end
