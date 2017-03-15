e953426f244deedde9aa6c78f65eb6553bc441a4
Alessandro Lepore <lepore.ale@gmail.com>
2017-03-15 19:43:48 +0100

    add dashboard widget form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,6 +36,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:name, :email, :licensor_id, :role)
+    params.require(:user).permit(:name, :email, :licensor_id, :role, { widgets: [] })
   end
 end
