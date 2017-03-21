05bc73723c92a68af14e7117df1d5e3e0d90e5d5
Alessandro Lepore <lepore.ale@gmail.com>
2017-03-21 17:28:59 +0100

    update post estimated value

--- app/models/facebook_post.rb
+++ app/models/facebook_post.rb
@@ -67,7 +67,7 @@ class FacebookPost < ApplicationRecord
 
   ransacker :status, formatter: proc { |status_name| statuses[status_name] }
 
-  ESTIMATED_VALUE = 60
+  ESTIMATED_VALUE = 120 # $
 
   def self.ransackable_scopes(auth_object = nil)
     [:with_any_domain]
