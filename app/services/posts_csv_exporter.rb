5cc27ce89e23c977b6b77ce59a8912685b4a9df2
Alessandro Lepore <lepore.ale@gmail.com>
2017-04-07 09:10:14 +0200

    add screenshot links fields to CSV export

--- app/services/posts_csv_exporter.rb
+++ app/services/posts_csv_exporter.rb
@@ -18,6 +18,8 @@ class PostsCSVExporter
           I18n.l(post.published_at, format: :compact),
           (I18n.l(post.blacklisted_at, format: :compact) if post.blacklisted_at),
           post.blacklisted_by,
+          post.ad_screenshots.map { |s| s.image.url }.join(" , "),
+          post.message,
           post.message,
           post.permalink,
           post.brand_names,
@@ -29,7 +31,8 @@ class PostsCSVExporter
           post.reported_to_facebook_by,
           (I18n.l(post.shut_down_by_facebook_at, format: :compact) if post.shut_down_by_facebook_at),
           post.facebook_report_number,
-          post.all_links.last
+          post.all_links.last,
+          post.product_screenshots.map { |s| s.image.url }.join(" , ")
         ]
       end
     end
@@ -46,6 +49,7 @@ class PostsCSVExporter
       published_at
       blacklisted_at
       blacklisted_by
+      link_to_ad_screenshot
       message
       link_to_ad_on_facebook
       brand_name
@@ -58,6 +62,7 @@ class PostsCSVExporter
       shut_down_by_facebook_at
       facebook_report_number
       link_to_product
+      link_to_product_screenshot
     ]
   end
 end
