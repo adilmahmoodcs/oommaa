1c7e95c20c21dcad90470fa81d0a149baef5908e
Alessandro Lepore <lepore.ale@gmail.com>
2017-04-10 23:23:14 +0200

    Removed AddScreenshotsForBlacklistedAds migration
    Migration are not really suitable for this kind of task, also my
    MigrateFacebookPageBrandsRelations was a bad idea.

--- db/migrate/20170404132225_add_screenshots_for_blacklisted_ads.rb
+++
@@ -1,8 +0,0 @@
-require 'rake' 
-class AddScreenshotsForBlacklistedAds < ActiveRecord::Migration[5.0]
-  def change
-    say_with_time "Adding Screen-shots for blacklisted ads"  do
-      Rake::Task['adding_screenshots_for_old_blacklisted_ads:add_screenshots'].invoke
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170404132225) do
+ActiveRecord::Schema.define(version: 20170329154634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
