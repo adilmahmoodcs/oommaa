21f59a546bc781b2faa5022be863349f4bc7f5fa
hammadKhan45 <hammad.masood@fliclicks.com>
2017-04-20 15:05:24 +0500

    Revert "added specs for assigned domains"
    This reverts commit cf78b0057c4901339a9730558bf99b9f1f3ecfed.

--- app/models/assigned_domain.rb
+++ app/models/assigned_domain.rb
@@ -20,9 +20,10 @@ class AssignedDomain < ApplicationRecord
 
   validates :user, :domain, presence: true
   validates_uniqueness_of :domain, scope: [:user]
-  validate :validate_user
+
+  before_create :validate_user
 
   def validate_user
-    errors.add(:error, "Cannot Assign to Admin User") if User.find(self.user_id).admin?
+    false if User.find(self.user_id).admin?
   end
 end
