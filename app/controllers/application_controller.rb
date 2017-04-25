2f73a8fca555c67bc4fc0a0093ebce3d9ccd4990
Hammad Khan <hammad.masood@fliclicks.com>
2017-04-25 10:56:25 +0500

    added confirmed user to switch users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < ActionController::Base
 
   before_action :authenticate_user!
   before_action :devise_permitted_parameters, if: :devise_controller?
+  before_action :set_variable_for_orignal_user
   # pundit checks
   after_action :verify_authorized, unless: :skip_autharization
   after_action :verify_policy_scoped, only: :index, unless: :devise_controller?
@@ -24,5 +25,23 @@ class ApplicationController < ActionController::Base
   def user_not_authorized
     flash[:alert] = "You are not authorized to perform this action."
     redirect_back(fallback_location: root_path)
+  end
+
+  def set_variable_for_orignal_user
+    if controller_name == 'switch_user' and action_name == "remember_user"
+      if params[:remember] == 'true'
+        session[:orignal_user_id] = current_user.id
+        session[:orignal_user_name] = current_user.name
+        session[:orignal_user_email] = current_user.email
+      elsif params[:remember] == 'false'
+        session.delete(:orignal_user_id)
+        session.delete(:orignal_user_name)
+        session.delete(:orignal_user_email)
+      end
+    end
+    puts "0000000000000000000000000000000000000000000000000000000000000000000000000000"
+    puts session[:orignal_user_id]
+    puts session[:orignal_user_name]
+    puts session[:orignal_user_email]
   end
 end
