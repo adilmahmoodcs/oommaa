4444b1c49c478124c517882e4d949f3ea8576954
Hammad Khan <hammad.masood@fliclicks.com>
2017-05-20 02:02:46 +0500

    fixed

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -14,5 +14,5 @@ server "dashboard.counterfind.com",
 set :sidekiq_processes, 2
 set :sidekiq_options_per_process, [
   "--queue default --queue not_facebook",
-  "--tag facebook --concurrency 1 --queue priority,5 --queue posts --queue pages"
+  "--tag facebook --concurrency 1 --queue priority,5 --queue posts --queue pages --mailer"
 ]
