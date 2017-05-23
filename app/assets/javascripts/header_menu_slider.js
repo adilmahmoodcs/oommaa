6c72629d6ebb488b3bf04ecc3ee5210a6ce0e2e8
Hammad Khan <hammad.masood@fliclicks.com>
2017-05-23 12:40:15 +0500

    assets precompile issues resolved

--- app/assets/javascripts/header_menu_slider.js
+++ app/assets/javascripts/header_menu_slider.js
@@ -1,23 +1,20 @@
 var SlickMenu = {
   init: function() {
     $('.slick_tabs').slick({
+      variableWidth: true,
+      arrows: true,
       infinite: false,
       speed: 300,
       slidesToShow: 7,
       slidesToScroll: 1,
-      variableWidth: true,
-      arrows: true,
-      responsive: [{
-        breakpoint: 769,
-        init: 'slick'
-        },{
-        breakpoint: 768.
-        settings: "unslick" // destroys slick
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            unslick: true
+          }
         }
-        ]
+      ]
     });
   }
 }
-
-
-
--- app/assets/javascripts/search_form.js
+++ app/assets/javascripts/search_form.js
@@ -12,26 +12,6 @@ var SearchForm = {
       variableWidth: true,
       arrows: true,
     });
-
-    $( window ).resize(function() {
-      if ($( window ).width() <= 768)
-      {
-        $(".slick_tabs").slick('unslick');
-      }
-
-      if ($( window ).width() == 769){
-        $('.slick_tabs').slick({
-          infinite: false,
-          speed: 300,
-          slidesToShow: 5,
-          slidesToScroll: 1,
-          variableWidth: true,
-          arrows: true,
-        });
-      }
-
-    });
-
   }
 }
 
