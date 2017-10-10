1a1fd6b6689dbaf556ccc5e0f6bf83e5e4d8ac6e
adilmamod <adilmahmood.cs@gmail.com>
2017-10-10 05:42:13 +0500

    Added has_many tranings in report

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -10,7 +10,10 @@ class ReportsController < ApplicationController
   def employees_data
     authorize :report, :team_production?
 
-    @users = User.admin
+    @q = policy_scope(Employee).ransack(params[:q])
+    @q.sorts = "name_case_insensitive asc" if @q.sorts.empty?
+    @employees = @q.result
+
     @from = Date.parse(params[:from]).beginning_of_day if params[:from].present?
     @to = Date.parse(params[:to]).end_of_day if params[:to].present?
   end
